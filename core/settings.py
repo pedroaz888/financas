debe69beb4390a40e05f94e1e7fa56b606b6f148
PedroAz <pdegutty@gmail.com>
2023-07-10 16:41:33 -0300

    railway

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-=p==8izr#swe%kkk=59zvady)#ngi1o3w8co3322yi%+b9j@f^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -68,10 +68,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
+DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'railway',
+            'USER': 'root',
+            'PASSWORD': 'AEeoGMVgehgac5Oe5MOP',
+            'HOST':'containers-us-west-149.railway.app',
+            'PORT':'6605',
     }
 }
 
--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('planejamento/', include('planejamento.urls')),
     path('contas/', include('contas.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
