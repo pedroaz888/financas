ff79a78be3b2319291fb00715fc1556022386134
PedroAz <pdegutty@gmail.com>
2023-07-11 14:49:05 -0300

    ok2

--- core/settings.py
+++ core/settings.py
@@ -131,17 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
-
-
-
-
-
 
 
 # Default primary key field type
