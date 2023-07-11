7286a8d690e3de66401ff44dbfff0ec67d761fd0
PedroAz <pdegutty@gmail.com>
2023-07-11 16:32:08 -0300

    ok5

--- core/settings.py
+++ core/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} 
+} """ 
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
@@ -91,7 +91,7 @@ DATABASES = {
         'PORT': '6605',
         
     }
-}   """
+}   
 
 
 # Password validation
